2b74916f3405603ef858815bb943ca0f97320608
Robert David <robert.david.public@gmail.com>
2014-03-10 12:03:39 +0100

    fix core/main.c wait.h on nonlinux

--- src/core/main.c
+++ src/core/main.c
@@ -58,7 +58,11 @@
 
 #include <stdlib.h>
 #include <sys/types.h>
+#ifdef __linux__
 #include <wait.h>
+#else
+#include <sys/wait.h>
+#endif
 #include <stdio.h>
 #include <string.h>
 #include <signal.h>
