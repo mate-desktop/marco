4134fe7aaf52512403e5a1319ed61fab0ce288d8
infirit <infirit@gmail.com>
2014-10-24 16:45:45 +0200

    keybindings.c: fix deprecated warning
    Based on metacity commit: 8f49828169efb43976e23dd15c6dc4d630346f50

--- src/core/keybindings.c
+++ src/core/keybindings.c
@@ -1274,7 +1274,7 @@ meta_display_process_key_event (MetaDisplay *display,
   /* window may be NULL */
 
 #ifdef HAVE_XKB
-  keysym = XKeycodeToKeysym (display->xdisplay, event->xkey.keycode, 0);
+  keysym = XkbKeycodeToKeysym (display->xdisplay, event->xkey.keycode, 0, 0);
   str = XKeysymToString (keysym);
 #else
   str = NULL;
