99e0041e07bb3efe3f4253d995a58e7f214df35f
Victor Kareh <vkareh@vkareh.net>
2017-10-23 16:31:52 -0400

    Add stricter logic for running without XPresent support

--- src/core/screen.c
+++ src/core/screen.c
@@ -740,16 +740,15 @@ list_windows (MetaScreen *screen)
                             children[i], &info->attrs);
 
       if (meta_error_trap_pop_with_return (screen->display, TRUE))
-        {
+	{
           meta_verbose ("Failed to get attributes for window 0x%lx\n",
                         children[i]);
-          g_free (info);
-          continue;
+	  g_free (info);
         }
       else
         {
-          info->xwindow = children[i];
-        }
+	  info->xwindow = children[i];
+	}
 
       result = g_list_prepend (result, info);
     }
