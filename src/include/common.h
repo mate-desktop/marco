b445ffd18323e2b256c2bf7dc3b079e44c475e8a
osch <oliver at luced de>
2019-04-15 00:07:46 +0200

    Limit icon size to 480

--- src/include/common.h
+++ src/include/common.h
@@ -303,7 +303,7 @@ struct _MetaButtonLayout
 /* should investigate changing these to whatever most apps use */
 #define META_DEFAULT_ICON_SIZE 48
 #define META_MIN_ICON_SIZE 6
-#define META_MAX_ICON_SIZE 1024
+#define META_MAX_ICON_SIZE 480
 
 #define META_MINI_ICON_WIDTH 16
 #define META_MINI_ICON_HEIGHT 16
