f1055793dcfd081840aab8dfca5606e230dfbdce
Victor Kareh <vkareh@redhat.com>
2019-04-15 15:16:40 -0400

    compositor: fix shadow size and placement
    upstream commit: https://gitlab.gnome.org/GNOME/metacity/commit/9fd053da

--- src/compositor/compositor-xrender.c
+++ src/compositor/compositor-xrender.c
@@ -605,8 +605,8 @@ shadow_clip (Display          *xdisplay,
              int               width,
              int               height)
 {
-  int shadow_dx = -1 * shadow_offsets_x [shadow_type];
-  int shadow_dy = -1 * shadow_offsets_y [shadow_type];
+  int shadow_dx = -1 * (int) shadow_offsets_x [shadow_type] - borders.invisible.left;
+  int shadow_dy = -1 * (int) shadow_offsets_y [shadow_type] - borders.invisible.top;
   XRectangle clip[4];
 
   /* Top */
@@ -1064,20 +1064,23 @@ win_extents (MetaCompWindow *cw)
             meta_frame_calc_borders (frame, &borders);
         }
 
-      cw->shadow_dx = shadow_offsets_x [cw->shadow_type];
-      cw->shadow_dy = shadow_offsets_y [cw->shadow_type];
+      cw->shadow_dx = (int) shadow_offsets_x [cw->shadow_type] + borders.invisible.left;
+      cw->shadow_dy = (int) shadow_offsets_y [cw->shadow_type] + borders.invisible.top;
 
       if (!cw->shadow)
         {
           double opacity = SHADOW_OPACITY;
+          int invisible_width = borders.invisible.left + borders.invisible.right;
+          int invisible_height = borders.invisible.top + borders.invisible.bottom;
+
           if (cw->opacity != (guint) OPAQUE)
             opacity = opacity * ((double) cw->opacity) / ((double) OPAQUE);
 
           cw->shadow = shadow_picture (display, screen, cw->shadow_type,
                                        opacity, cw->alpha_pict,
                                        borders,
-                                       cw->attrs.width + cw->attrs.border_width * 2,
-                                       cw->attrs.height + cw->attrs.border_width * 2,
+                                       cw->attrs.width - invisible_width + cw->attrs.border_width * 2,
+                                       cw->attrs.height - invisible_height + cw->attrs.border_width * 2,
                                        &cw->shadow_width, &cw->shadow_height);
         }
 
