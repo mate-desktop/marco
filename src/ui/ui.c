b925581e37b2ac2ffdb97469fbfb284aac93feab
Victor Kareh <vkareh@redhat.com>
2019-05-22 11:12:24 -0400

    frame: Add "get_corner_radiuses" chain
    https://bugzilla.gnome.org/show_bug.cgi?id=628195
    
    upstream commit:
    https://gitlab.gnome.org/GNOME/metacity/commit/3994d7a0

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -302,6 +302,19 @@ meta_ui_get_frame_borders (MetaUI *ui,
   meta_frames_get_borders (ui->frames, frame_xwindow, borders);
 }
 
+void
+meta_ui_get_corner_radiuses (MetaUI *ui,
+                             Window  xwindow,
+                             float  *top_left,
+                             float  *top_right,
+                             float  *bottom_left,
+                             float  *bottom_right)
+{
+  meta_frames_get_corner_radiuses (ui->frames, xwindow,
+                                   top_left, top_right,
+                                   bottom_left, bottom_right);
+}
+
 static void
 set_background_none (Display *xdisplay,
                      Window   xwindow)
