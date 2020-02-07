e21d90733eeef1d9156f601e70db868012cbcc56
Victor Kareh <vkareh@redhat.com>
2020-02-07 14:01:20 -0500

    let the ui layer (via the core) construct the frame mask
    upstream commit:
    https://gitlab.gnome.org/GNOME/metacity/commit/0de99772

--- src/core/frame.c
+++ src/core/frame.c
@@ -338,19 +338,6 @@ update_shape (MetaFrame *frame)
     return FALSE;
 }
 
-void
-meta_frame_get_corner_radiuses (MetaFrame *frame,
-                                float     *top_left,
-                                float     *top_right,
-                                float     *bottom_left,
-                                float     *bottom_right)
-{
-  meta_ui_get_corner_radiuses (frame->window->screen->ui,
-                               frame->xwindow,
-                               top_left, top_right,
-                               bottom_left, bottom_right);
-}
-
 gboolean
 meta_frame_sync_to_window (MetaFrame *frame,
                            int        resize_gravity,
@@ -411,6 +398,14 @@ meta_frame_get_frame_bounds (MetaFrame *frame)
                                    frame->xwindow,
                                    frame->rect.width,
                                    frame->rect.height);
+}
+
+void
+meta_frame_get_mask (MetaFrame *frame,
+                     cairo_t   *cr)
+{
+  meta_ui_get_frame_mask (frame->window->screen->ui, frame->xwindow,
+                          frame->rect.width, frame->rect.height, cr);
 }
 
 void
