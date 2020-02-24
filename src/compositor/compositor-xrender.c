4ae91ff847a9d4c78dbe9eb882f3bba36f7493ef
rcaridade145 <rcaridade145@gmail.com>
2020-02-24 10:42:20 +0000

    Guard against null changes introduced by previous commit

--- src/compositor/compositor-xrender.c
+++ src/compositor/compositor-xrender.c
@@ -645,7 +645,7 @@ shadow_picture_clip (Display          *xdisplay,
 
   visible_region = meta_window_get_frame_bounds (cw->window);
 
-  if (!visible_region)
+  if (visible_region == NULL)
     return;
 
   shadow_dx = -1 * (int) shadow_offsets_x [cw->shadow_type] - borders.invisible.left;
@@ -1153,9 +1153,13 @@ border_size (MetaCompWindow *cw)
     {
       visible_region = meta_window_get_frame_bounds (cw->window);
 
-      if (visible_region)
+      if (visible_region != NULL) {
         visible = cairo_region_to_xserver_region (xdisplay, visible_region);
+      }
+      else {
+        g_warning ("Visible region is null");
     }
+  }
 
   meta_error_trap_push (display);
   border = XFixesCreateRegionFromWindow (xdisplay, cw->id,
